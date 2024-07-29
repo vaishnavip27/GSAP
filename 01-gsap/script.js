d922436fb7e21db864beca3e7c69718f7f50a5bb
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-29 11:51:54 +0530

    commit

--- 01-gsap/script.js
+++ 01-gsap/script.js
@@ -1,21 +1,41 @@
-gsap.to("#box1", {
-  x: 1500,
+// gsap.to("#box1", {
+//   x: 1500,
+//   duration: 1.5,
+//   delay: 1,
+//   rotate: 360,
+// });
+
+// gsap.to("#box2", {
+//   x: 1500,
+//   backgroundColor: "yellow",
+//   duration: 1.5,
+//   delay: 2.5,
+// });
+
+// gsap.to("#box3", {
+//   x: 1500,
+//   scale: 0.5,
+//   borderRadius: "50%",
+//   duration: 1.5,
+//   delay: 4,
+// });
+
+var tl = gsap.timeline();
+
+tl.to("#box1", {
+  x: 1200,
+  rotate: 360,
   duration: 1.5,
   delay: 1,
-  rotate: 360,
 });
 
-gsap.to("#box2", {
-  x: 1500,
-  backgroundColor: "yellow",
+tl.to("#box2", {
+  x: 1200,
   duration: 1.5,
-  delay: 2.5,
 });
 
-gsap.to("#box3", {
-  x: 1500,
-  scale: 0.5,
+tl.to("#box3", {
+  x: 1200,
+  duration: 1.5,
   borderRadius: "50%",
-  duration: 1.5,
-  delay: 3.5,
 });
