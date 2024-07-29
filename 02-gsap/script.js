e1c320a92580635ebc8a09820821602d1df3d44e
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-30 02:57:01 +0530

    commit

---
+++ 02-gsap/script.js
@@ -0,0 +1,21 @@
+gsap.from("#page1 #box", {
+  scale: 0,
+  duration: 2,
+  rotate: 360,
+});
+
+gsap.from("#page2 #box", {
+  scale: 0,
+  delay: 2,
+  duration: 2,
+  rotate: 360,
+  scrollTrigger: {
+    trigger: "#page2 #box",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+    end: "top 30%",
+    scrub: 2,
+    pin: true,
+  },
+});
