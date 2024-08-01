76c53bed8a0a1012b0c41be17f48530d94ed0c29
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-08-01 07:32:03 +0530

    commit

--- 08-gsap/src/components/element-2/element.js
+++ 08-gsap/src/components/element-2/element.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./element.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Element() {
+  //to animate each element we can't give ID to each element, so we'll use a ref to track the elements
+  // const boxRef = useRef();
+  // useGSAP(() => {
+  //   gsap.from(boxRef.current, {
+  //     rotate: 360,
+  //     delay: 0.5,
+  //     duration: 1,
+  //     scale: 0,
+  //     opacity: 0,
+  //   });
+  // });
+
+  //Scope concept
+  useGSAP(
+    () => {
+      gsap.from(".box", {
+        rotate: 360,
+        scale: 0,
+        duration: 1,
+        opacity: 0,
+        delay: 0.5,
+      });
+    },
+    { scope: ".container" }
+  );
+
   return (
     <main>
       <div className="container">
